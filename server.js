be2516873959f52d8befa87d81a247f0795c5dc4
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-27 23:52:07 -0800

    implement scrambleWord

--- server.js
+++ server.js
@@ -19,7 +19,68 @@ const gameSchema = new mongoose.Schema({
 });
 const Game = mongoose.model('Game', gameSchema);
 
-const port = process.env.PORT | 3000;
+const scrambleWord = (word) => {
+        // Convert the word into an array of characters
+        let characters = word.split('');
+    
+        // Fisher-Yates Shuffle Algorithm
+        for (let i = characters.length - 1; i > 0; i--) {
+            // Pick a random index before the current one
+            let j = Math.floor(Math.random() * (i + 1));
+    
+            // Swap characters[i] with the character at the random index
+            [characters[i], characters[j]] = [characters[j], characters[i]];
+        }
+    
+        // Convert the array of characters back into a string
+        return characters.join('');
+}
+    
+  
+const findMismatches = (solution, playerSolution) => {
+// Implement logic to find mismatches here
+};
+
+app.get('/new-game', async (req, res) => {
+    try {
+      const word = await generateWord();
+      const picture = await generatePicture(word);
+      const scramble = scrambleWord(word);
+  
+      const newGame = new Game({ solution: word, scramble, picture });
+      await newGame.save();
+  
+      res.send({ gameId: newGame._id, scramble, picture });
+    } catch (error) {
+      res.status(500).send('Error creating new game');
+    }
+});
+
+app.get('/check-answer', async (req, res) => {
+    const { playerSolution, gameId } = req.query;
+  
+    try {
+      const game = await Game.findById(gameId);
+      if (!game) {
+        res.status(404).send('Game not found');
+        return;
+      }
+  
+      const checkResult = game.solution === playerSolution;
+      let mismatches = [];
+      if (!checkResult) {
+        mismatches = findMismatches(game.solution, playerSolution);
+      }
+  
+      res.send({ checkResult, mismatches });
+    } catch (error) {
+      res.status(500).send('Error checking answer');
+    }
+});
+
+
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
