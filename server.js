72476109c328dcd1e6a78ff7ee34e7ca6a4c0ca4
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-11-27 23:39:41 -0800

    simplify structure

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { generateWord, generatePicture } from './OpenAIAPI.js'; // Adjust this import based on your implementation
 import dotenv from 'dotenv';
-
 dotenv.config();
+
 
 const app = express();
 app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define the Game model
+const gameSchema = new mongoose.Schema({
+  solution: String,
+  scramble: String,
+  picture: String
+});
+const Game = mongoose.model('Game', gameSchema);
+
+const port = process.env.PORT | 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
