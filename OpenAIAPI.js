0a19ecc01383c3785446be865e6343eb9dbd229b
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-12-08 01:14:10 -0800

    correct object reference

--- OpenAIAPI.js
+++ OpenAIAPI.js
@@ -13,9 +13,9 @@ class OpenAIAPI {
     let picture;
     while(!success){
       try{
-        const word = await OpenAIAPI_obj.generateWord();
+        const word = await this.generateWord();
         console.log({word});
-        const picture = await OpenAIAPI_obj.generatePicture(word);
+        const picture = await this.generatePicture(word);
         console.log({picture});
         success = true;
       }
