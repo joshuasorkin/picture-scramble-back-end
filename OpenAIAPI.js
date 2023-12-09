d46a4806cd32ad13d7a6b2ee0bd08c3c95708574
joshuasorkin <joshua.d.sorkin@gmail.com>
2023-12-09 10:28:47 -0800

    prompt tuning

--- OpenAIAPI.js
+++ OpenAIAPI.js
@@ -34,10 +34,11 @@ class OpenAIAPI {
   async generateWord() {
     try {
       console.log("generating word...");
+      
       const response = await this.client.chat.completions.create({
         model: "gpt-3.5-turbo", // Or another suitable model
         messages: [
-          {role:"user",content:"Generate a random English language word between 4 to 16 characters in length.  Do not surround it with quotation marks. Respond only with the word itself."}
+          {role:"user",content:process.env.GENERATE_WORD_PROMPT}
         ],
         max_tokens: 10,
       });
@@ -56,7 +57,7 @@ class OpenAIAPI {
     try {
       const response = await this.client.images.generate({
         model: "dall-e-3", // Replace with the appropriate model
-        prompt:`Generate a picture of ${word}, photorealistic.`,
+        prompt:`Generate a picture of ${word}, photorealistic.  Do not include the word '${word}' in the picture.`,
         n: 1, // Number of images to generate
         size: "1024x1024" // Image size
       });
