64142714c542e0d78c06c7875d624e0ce48c7e50
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-01-07 09:45:05 -0800

    disable alreadyshowntoday

--- OpenAIAPI.js
+++ OpenAIAPI.js
@@ -33,6 +33,7 @@ class OpenAIAPI {
         word = await this.generateWord(wordParam,score,language);
         console.log({word});
         alreadyShownToday = await this.wordGeneratedToday(word);
+        alreadyShownToday = false;
         console.log({alreadyShownToday});
         if (alreadyShownToday){
           generateAttempts++;
