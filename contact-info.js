6b342de342680f792ded9615e3d14ea1588ef8cd
joshuasorkin <joshua.d.sorkin@gmail.com>
2024-03-24 03:10:23 -0700

    add create contact functionality

--- contact-info.js
+++ contact-info.js
@@ -9,6 +9,15 @@ class ContactInfo{
             instagram:process.env.DEFAULT_INSTAGRAM
         }
     }
+
+    createContact(name,phone,email,platform,handle){
+        const contact = {};
+        contact["name"]=name;
+        contact["phone"]=phone;
+        contact["email"]=email;
+        contact["platform"]=handle;
+        return contact;
+    }
 }
 
 export default ContactInfo;
